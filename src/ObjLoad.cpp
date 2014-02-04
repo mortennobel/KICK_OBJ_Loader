d83f314f95881686d936c49f1898eda8c7632455
Morten Nobel-Jørgensen <morten@nobel-joergensen.com>
2014-02-04 14:35:33 +0900

    Update ObjLoad.cpp

--- src/ObjLoad.cpp
+++ src/ObjLoad.cpp
@@ -55,6 +55,7 @@ namespace kick {
         return res;
     }
     
+    // replace first occurrence of from in str with to
     bool replace(std::string& str, const std::string& from, const std::string& to) {
         size_t start_pos = str.find(from);
         if(start_pos == std::string::npos)
